100b52c72fb5c3df24e94d05e68aa7666ef0f207
RehanGith <chrehan1007@gmail.com>
2025-02-22 16:21:44 +0500

    Created createdUser function in viewModel

--- app/src/main/java/com/example/my_shoppings/util/Response.kt
+++ app/src/main/java/com/example/my_shoppings/util/Response.kt
@@ -3,6 +3,6 @@ package com.example.my_shoppings.util
 sealed class Response<T>(val data: T? = null,
     val message: String? = null) {
     class Success<T>(data: T) : Response<T>(data)
-    class Error<T>(message: String, data: T? = null) : Response<T>(data, message)
+    class Error<T>(message: String) : Response<T>( null,message)
     class Loading<T> : Response<T>()
 }
--- app/src/main/java/com/example/my_shoppings/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/my_shoppings/viewModel/LoginViewModel.kt
@@ -2,8 +2,12 @@ package com.example.my_shoppings.viewModel
 
 import android.view.View
 import androidx.lifecycle.ViewModel
+import com.example.my_shoppings.model.User
+import com.example.my_shoppings.util.Response
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
@@ -11,5 +15,18 @@ import javax.inject.Inject
 class LoginViewModel @Inject constructor(
     private val firebaseAuth: FirebaseAuth
 ) : ViewModel(){
-    private val _register = MutableStateFlow()
+    private var _register = MutableStateFlow<Response<FirebaseUser>>(Response.Loading())
+    val register : Flow<Response<FirebaseUser>>
+        get() = _register
+
+    fun createAccount(user : User , password : String){
+        firebaseAuth.createUserWithEmailAndPassword(user.email, password)
+            .addOnSuccessListener {
+                it.user?.let {
+                    _register.value = Response.Success(it)
+                }
+            }.addOnFailureListener {
+                _register.value = Response.Error(it.message.toString())
+            }
+    }
 }
