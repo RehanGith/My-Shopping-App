29bed652d8717a7d5cd4ae741c18ce8f37a94c7c
RehanGith <chrehan1007@gmail.com>
2025-02-22 17:04:19 +0500

    coded logic when register button will be clicked

--- app/src/main/java/com/example/my_shoppings/fragments/login/RegisterFragment.kt
+++ app/src/main/java/com/example/my_shoppings/fragments/login/RegisterFragment.kt
@@ -1,7 +1,56 @@
 package com.example.my_shoppings.fragments.login
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.my_shoppings.R
+import com.example.my_shoppings.databinding.FragmentRegisterBinding
+import com.example.my_shoppings.model.User
+import com.example.my_shoppings.util.Response
+import com.example.my_shoppings.viewModel.RegisterViewModel
+import kotlinx.coroutines.launch
 
 class RegisterFragment : Fragment(R.layout.fragment_register){
+    private lateinit var binding: FragmentRegisterBinding
+    private val registerViewModel by viewModels<RegisterViewModel>()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRegisterBinding.bind(view)
+
+        binding.apply {
+            buttonRegisterRegister.setOnClickListener {
+                val user = User(
+                    edFirstNameRegister.text.toString(),
+                    edLastNameRegister.text.toString(),
+                    edEmailRegister.text.toString()
+                )
+                val password = edPasswordRegister.text.toString()
+                registerViewModel.createAccount(user, password)
+
+            }
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                registerViewModel.register.collect { response ->
+                    when(response) {
+                        is Response.Error -> {
+                            Log.d("main", response.message.toString())
+                            binding.buttonRegisterRegister.revertAnimation()
+                        }
+                        is Response.Loading -> binding.buttonRegisterRegister.startAnimation()
+                        is Response.Success -> {
+                            Log.d("main", response.data.toString())
+                            binding.buttonRegisterRegister.revertAnimation()
+                        }
+                    }
+                }
+            }
+        }
+
+    }
 }
