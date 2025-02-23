786eee9da854196f002b717500ceedd6bf7e12b2
RehanGith <chrehan1007@gmail.com>
2025-02-23 17:54:28 +0500

    corrected bug, i wasn't coverting edittext to string

--- app/src/main/java/com/example/my_shoppings/dialogs/BottomDialog.kt
+++ app/src/main/java/com/example/my_shoppings/dialogs/BottomDialog.kt
@@ -1,5 +1,7 @@
 package com.example.my_shoppings.dialogs
 
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.my_shoppings.R
@@ -14,18 +16,29 @@ fun Fragment.setUpBottomDialog(
     dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
     dialog.show()
 
-    val edEmail = view?.findViewById<EditText>(R.id.edResetPassword)?.text?.trim()
-    val btnCancel = view?.findViewById<EditText>(R.id.buttonCancelResetPassword)
-    val btnReset = view?.findViewById<EditText>(R.id.buttonSendResetPassword)
+    val edEmail = dialog.findViewById<EditText>(R.id.edResetPassword)
+    val btnReset = dialog.findViewById<Button>(R.id.buttonSendResetPassword)
+    val btnCancel = dialog.findViewById<Button>(R.id.buttonCancelResetPassword)
 
     if (btnCancel != null) {
         btnCancel.setOnClickListener {
             dialog.dismiss()
+
         }
+    } else {
+        Log.d("TAG", "setUpBottomDialog: btnCancel is null")
     }
 
-    btnReset?.setOnClickListener {
-        onSendClick(edEmail.toString())
-        dialog.dismiss()
+    if (btnReset != null) {
+        if(edEmail != null) {
+            btnReset.setOnClickListener {
+                onSendClick(edEmail.text.toString())
+                dialog.dismiss()
+            }
+        } else {
+            Log.d("TAG", "setUpBottomDialog: edEmail is null")
+        }
+    } else {
+        Log.d("TAG", "setUpBottomDialog: btnReset is null")
     }
 }
