d598340a39b272974694cbef373c251e029a6783
RehanGith <chrehan1007@gmail.com>
2025-02-24 00:43:12 +0500

    implemeted the bottom navigation with shopping activity

--- app/src/main/java/com/example/my_shoppings/app/ShoppingActivity.kt
+++ app/src/main/java/com/example/my_shoppings/app/ShoppingActivity.kt
@@ -3,15 +3,23 @@ package com.example.my_shoppings.app
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.my_shoppings.R
+import com.example.my_shoppings.databinding.ActivityShoppingBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ShoppingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityShoppingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_shopping)
+        binding = ActivityShoppingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setupWithNavController(navController)
 
     }
 }
