f976e2bdfcf86680022c1a9cd2de99254c27a9c8
RehanGith <chrehan1007@gmail.com>
2025-02-23 23:55:49 +0500

    Connected the HomeFragment with fragment_home layout

---
+++ app/src/main/java/com/example/my_shoppings/fragments/shopping/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.my_shoppings.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.my_shoppings.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+}
