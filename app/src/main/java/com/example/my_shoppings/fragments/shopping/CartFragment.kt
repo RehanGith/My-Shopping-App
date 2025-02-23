0115191de3bf9052709d3bf86e050a3135ce0a3c
RehanGith <chrehan1007@gmail.com>
2025-02-23 23:56:52 +0500

    Connect cart frgament with layout of cart

---
+++ app/src/main/java/com/example/my_shoppings/fragments/shopping/CartFragment.kt
@@ -0,0 +1,7 @@
+package com.example.my_shoppings.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.my_shoppings.R
+
+class CartFragment : Fragment(R.layout.fragment_cart) {
+}
