0f67cb3eebc5eb33f2ded87fde5c3307cf02c327
RehanGith <chrehan1007@gmail.com>
2025-03-02 22:08:54 +0500

    Enabled binding in HomeFragment

--- app/src/main/java/com/example/my_shoppings/fragments/shopping/HomeFragment.kt
+++ app/src/main/java/com/example/my_shoppings/fragments/shopping/HomeFragment.kt
@@ -1,7 +1,18 @@
 package com.example.my_shoppings.fragments.shopping
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.my_shoppings.R
+import com.example.my_shoppings.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+
+
+    }
 }
