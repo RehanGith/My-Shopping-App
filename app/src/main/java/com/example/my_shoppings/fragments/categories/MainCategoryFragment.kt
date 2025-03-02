dcc6abc3cfdf4c8f22515738646834f145c3bc10
RehanGith <chrehan1007@gmail.com>
2025-03-02 23:47:16 +0500

    Allowed data binding in BaseCategoryFragment

--- app/src/main/java/com/example/my_shoppings/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/example/my_shoppings/fragments/categories/MainCategoryFragment.kt
@@ -1,6 +1,7 @@
 package com.example.my_shoppings.fragments.categories
 
 import androidx.fragment.app.Fragment
+import com.example.my_shoppings.R
 
-class MainCategoryFragment : Fragment() {
+class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
 }
