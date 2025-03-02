cc9aa957a43c2e73904138ddfb176b5f4688f158
RehanGith <chrehan1007@gmail.com>
2025-03-02 23:10:51 +0500

    made some changes in homeFragment

--- app/src/main/java/com/example/my_shoppings/fragments/shopping/HomeFragment.kt
+++ app/src/main/java/com/example/my_shoppings/fragments/shopping/HomeFragment.kt
@@ -5,14 +5,27 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.my_shoppings.R
 import com.example.my_shoppings.databinding.FragmentHomeBinding
+import com.example.my_shoppings.fragments.categories.AccessoryFragment
+import com.example.my_shoppings.fragments.categories.CupboardFragment
+import com.example.my_shoppings.fragments.categories.FurnitureFragment
+import com.example.my_shoppings.fragments.categories.MainCategoryFragment
+import com.example.my_shoppings.fragments.categories.TableFragment
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var binding: FragmentHomeBinding
-
+    private val titlesCategory = listOf("Home", "Chair", "Cupboard", "Table", "Accessory", "Furniture")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
-
+        val categoryFragments = listOf(
+            MainCategoryFragment(),
+            CartFragment(),
+            CupboardFragment(),
+            TableFragment(),
+            AccessoryFragment(),
+            FurnitureFragment()
+        )
 
     }
+
 }
